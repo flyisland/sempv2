916d8a25117e3b52a15215edbba6c262996725b4
flyisland <flyisland@gmail.com>
2019-06-30 18:45:19 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Backing Up and Restoring Solace PubSub+ VPN Configs with SEMPv2
+
+## Usage
+
+## Reference
+
+* [How To Package Your Python Code](https://python-packaging.readthedocs.io/en/latest/index.html)
+* [Setuptools Integration](https://click.palletsprojects.com/en/7.x/setuptools/#setuptools-integration)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='sempv2',
+      version='0.1',
+      description='Backing Up and Restoring Solace PubSub+ VPN Configs with SEMPv2',
+      url='http://github.com/flyisland/sempv2',
+      author='Island Chen',
+      author_email='island.chen@outlook.com',
+      license='MIT',
+      packages=['sempv2'],
+      install_requires=[
+          'click',
+      ],
+      entry_points='''
+            [console_scripts]
+            sempv2=sempv2.cmd:cli
+      ''',
+      zip_safe=False)
