0a764c308a6bc9701a822835a37c27d46734627b
flyisland <flyisland@gmail.com>
2020-05-16 10:42:22 +0800

    feat: add support for cluster

--- sempv2/cmd.py
+++ sempv2/cmd.py
@@ -35,8 +35,15 @@ def cli(ctx, admin_user, admin_password, host, verbose):
 
 @cli.group()
 def vpn():
-    """Backing Up and Restoring Solace PubSub+ VPN"""
+    """Backing Up and Restoring the Setting of PubSub+ VPN"""
     pass
+
+@cli.group()
+def cluster():
+    """Backing Up and Restoring the Setting of PubSub+ DMR Cluster"""
+    pass
+
+# ------------- sub commands of vpn -------------
 
 @vpn.command(name="backup")
 @click.argument('vpn_name')
@@ -70,6 +77,42 @@ def restore_vpn( config_file, curl_command):
 def update_vpn(config_file, curl_command, update_password):
     """Update the VPN with the configuration file"""
     update("msgVpns", config_file, curl_command, update_password)
+
+# ------------- sub commands of cluster -------------
+
+@cluster.command(name="backup")
+@click.argument('cluster_name')
+def backup_cluster(cluster_name):
+    """Fetches the whole configuration of a Cluster"""
+    backup("dmrClusters", cluster_name)
+
+@cluster.command(name="delete")
+@click.confirmation_option()
+@click.argument('cluster_name')
+@click.option('-c', '--curl-command', default=False, show_default=True, is_flag=True,
+    help='Output curl commands only')
+def delete_cluster(cluster_name, curl_command):
+    """Delete the Cluster"""
+    delete("dmrClusters", cluster_name, curl_command)
+
+@cluster.command(name="restore")
+@click.argument('config-file', type=click.Path(exists=True))
+@click.option('-c', '--curl-command', default=False, show_default=True, is_flag=True,
+    help='Output curl commands only')
+def restore_cluster( config_file, curl_command):
+    """Restore the Cluster with the configuration file"""
+    restore("dmrClusters", config_file, curl_command)
+
+@cluster.command(name="update")
+@click.argument('config-file', type=click.Path(exists=True))
+@click.option('-c', '--curl-command', default=False, show_default=True, is_flag=True,
+    help='Output curl commands only')
+@click.option('-u', '--update-password', default=False, show_default=True, is_flag=True,
+    help='Whether to update passwords')
+def update_cluster(config_file, curl_command, update_password):
+    """Update the Cluster with the configuration file"""
+    update("dmrClusters", config_file, curl_command, update_password)
+
 
 
 if __name__ == '__main__':
